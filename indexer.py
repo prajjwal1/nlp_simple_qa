31ba954936d46087ee4e00601df0b59684f567aa
CrunchyCat <calebrhoff@gmail.com>
2021-11-29 13:09:47 -0600

    simplify schema

--- indexer.py
+++ indexer.py
@@ -9,9 +9,9 @@ solr = pysolr.Solr('http://localhost:8983/solr/', always_commit=True, timeout=10
 # Load File Title & Text from Articles Folder into Entries List
 entries = []
 for filename in os.listdir(LOC_ARTICLES):
-  entry = { "id": filename.split('.')[0] }  # id = title
+  entry = { "id": filename.split('.')[0] }
   with open(f'{LOC_ARTICLES}/{filename}') as f:
-    entry['title'] = f.readlines()          # title = text
+    entry['text'] = f.readlines()
   entries.append(entry)
 
 # Add Entries List to the Indexer
