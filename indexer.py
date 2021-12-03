7b38ab837d2d3c196d84716874e16023f3daf4c4
Caleb Hoff <crunchycat1@gmail.com>
2021-12-03 15:59:50 -0600

    fix for solr

--- indexer.py
+++ indexer.py
@@ -1,7 +1,7 @@
 import pysolr
 import os
 
-LOC_ARTICLES = 'articles/'             # Where Articles are Stored to be Indexed
+LOC_ARTICLES = 'data/articles/'             # Where Articles are Stored to be Indexed
 
 # Create a Client Instance
 solr = pysolr.Solr('http://localhost:8983/solr/', always_commit=True, timeout=10)
@@ -9,8 +9,9 @@ solr = pysolr.Solr('http://localhost:8983/solr/', always_commit=True, timeout=10
 # Load File Title & Text from Articles Folder into Entries List
 entries = []
 for filename in os.listdir(LOC_ARTICLES):
+  print(filename)
   entry = { "id": filename.split('.')[0] }
-  with open(f'{LOC_ARTICLES}/{filename}') as f:
+  with open(f'{LOC_ARTICLES}/{filename}', 'r', encoding="utf8") as f:
     entry['text'] = f.readlines()
   entries.append(entry)
 
